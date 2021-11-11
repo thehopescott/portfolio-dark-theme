425f84e2faaff94cc3880fdfa85a36689ba3eccd
astrus <hopescott@protonmail.com>
2021-11-11 23:17:32 +0200

    Added 'Contact' and 'About' sections and finalised navigation

--- js/app.js
+++ js/app.js
@@ -1,38 +1,54 @@
 const projectsLink = document.querySelector("#proLink");
 const testLink = document.querySelector("#tesLink");
 const aboutLink = document.querySelector("#aboutLink");
+const contactLinks = document.querySelectorAll(".hire-me");
 
 let mainContainer = document.querySelector("main");
 let homeContainer = document.querySelector("#home");
+let aboutContainer = document.querySelector("#aboutme");
 let projectsContainer = document.querySelector("#projects");
 let testsContainer = document.querySelector("#testimonies");
-let aboutContainer = document.querySelector("#aboutme");
+let contactContainer = document.querySelector("#contactme");
 let footr = document.querySelector("footer");
 
 aboutContainer.remove();
 projectsContainer.remove();
 testsContainer.remove();
+contactContainer.remove();
 footr.remove();
 
 projectsLink.addEventListener("click", (e) => {
-    e.target.style.color = "#fff";
     homeContainer.remove();
     testsContainer.remove();
+    aboutContainer.remove();
+    contactContainer.remove();
     mainContainer.append(projectsContainer);
     mainContainer.append(footr);
 });
 testLink.addEventListener("click", (e) => {
-    e.target.style.color = "#fff";
     homeContainer.remove();
     projectsContainer.remove();
+    aboutContainer.remove();
+    contactContainer.remove();
     mainContainer.append(testsContainer);
     mainContainer.append(footr);
 });
 aboutLink.addEventListener("click", (e) => {
-    e.target.style.color = "#fff";
     homeContainer.remove();
     projectsContainer.remove();
+    contactContainer.remove();
     testsContainer.remove();
     mainContainer.append(aboutContainer);
     mainContainer.append(footr);
 });
+
+for (let i = 0; i < contactLinks.length; i++) {
+    contactLinks[i].addEventListener("click", (e) => {
+        homeContainer.remove();
+        projectsContainer.remove();
+        aboutContainer.remove();
+        testsContainer.remove();
+        mainContainer.append(contactContainer);
+        mainContainer.append(footr);
+    });
+}
